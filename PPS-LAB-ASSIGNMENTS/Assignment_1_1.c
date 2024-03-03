6ab7fa9077a441c505e41cc6d7863b8289689c3d
Jagannath Hansda <jagannath4698@gmail.com>
2024-03-03 19:45:08 +0530

    Added Assignement 1 of 1

---
+++ PPS-LAB-ASSIGNMENTS/Assignment_1_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int num1=10;
+    int num2=20;
+    float float1=500.50;
+    char char1='A';
+    printf("%d , %d , %f , %c",num1,num2,float1,char1);
+    return 0;
+}
