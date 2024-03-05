b31b765295544aa159fa19e6f363afc0706492c1
Jagannath Hansda <jagannath4698@gmail.com>
2024-03-05 23:05:08 +0530

    Question + Code for Q2 of Assignment 2

---
+++ PPS-LAB-ASSIGNMENTS/Assignment_2/Question_2.c
@@ -0,0 +1,26 @@
+/*
+
+2. Write a program whether a given year is a leap year or not.
+
+*/
+
+#include <stdio.h>
+int main()
+{
+    int year;
+    printf("Enter a Year :");
+    scanf("%d",&year);
+    if (year%4==0 && year%100!=0)
+    {
+        printf("%d is a LEAP year",year);
+    }
+    else if(i%400==0)
+    {
+        printf("%d is a LEAP year",year);
+    }
+    else
+    {
+        printf("%d is not a LEAP Year",year);
+    }
+    return 0;
+}
